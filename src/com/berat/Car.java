5b14158bc6a1d55f454f863fb00097ce09a87191
beratfurkanc <beratfurkanc@hotmail.com>
2019-08-06 22:40:31 -0400

    dsadasdsa

--- src/com/berat/Car.java
+++ src/com/berat/Car.java
@@ -2,11 +2,11 @@ package com.berat;
 
 public class Car {
     public static void main(String[] args) {
-<<<<<<< HEAD
+
         System.out.println("I like cars");
         System.out.println("different things");
-=======
+
         System.out.println("I like carsssssssssssssssssssssssssssssss");
->>>>>>> ff48e374f00363b575a13bb7581cb7c93f015bcc
+
     }
 }
