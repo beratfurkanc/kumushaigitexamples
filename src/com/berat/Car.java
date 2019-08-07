3f06c16c308fb4ce9bca19531c757f06e1275a22
beratfurkanc <beratfurkanc@hotmail.com>
2019-08-06 22:44:49 -0400

    dasdsadas

--- src/com/berat/Car.java
+++ src/com/berat/Car.java
@@ -3,16 +3,9 @@ package com.berat;
 public class Car {
     public static void main(String[] args) {
 
-        System.out.println("I like cars");
-        System.out.println("different things");
 
-        System.out.println("I like carsssssssssssssssssssssssssssssss");
-
-
-        System.out.println("fixed");
-
-
-        System.out.println("kumushai");
+        int a=5;
+        System.out.println("inumbersss");
 
     }
 }
